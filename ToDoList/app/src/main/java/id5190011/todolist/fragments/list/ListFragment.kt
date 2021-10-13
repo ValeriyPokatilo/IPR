1499bb2f2425d3fcbfd01d6b56cef80244597d0c
Valeriy Pokatilo <v.pokatilo@movavi.com>
2021-10-13 22:09:01 +0700

    Создание меню (урок 9)

--- ToDoList/app/src/main/java/id5190011/todolist/fragments/list/ListFragment.kt
+++ ToDoList/app/src/main/java/id5190011/todolist/fragments/list/ListFragment.kt
@@ -2,10 +2,8 @@ package id5190011.todolist.fragments.list
 
 import android.os.Bundle
 import android.text.Layout
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -31,6 +29,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set menu
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
