e84c984bee7a16f2dbdc91a337258c6b2cf61fc3
Valeriy Pokatilo <v.pokatilo@movavi.com>
2021-10-16 13:42:27 +0700

    Создание viewModel (урок 21)

---
+++ ToDoList/app/src/main/java/id5190011/todolist/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,30 @@
+package id5190011.todolist.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import id5190011.todolist.data.ToDoDatabase
+import id5190011.todolist.data.models.ToDoData
+import id5190011.todolist.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application): AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDataBase(application).toDoDao()
+    private val repository: ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
